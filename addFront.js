6cbe0e7bd07c6446efa9ddc05fab72504f8dbe66
Jordan McGhee <jordanmcghee95@gmail.com>
2021-06-13 17:04:30 -0400

    initial commit

---
+++ addFront.js
@@ -0,0 +1,52 @@
+// FRONTS
+
+// creates node class to reference/instantiate in SLL class
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class SLL {
+    constructor(){
+        // creates empty list, the head of the list is null by default
+        this.head = null
+    }
+
+    // Add Front
+    // Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
+    addFront(value) {
+        // instantiate a new node using our class
+        let newNode = new Node(value)
+        // our new node's next will be the current head of the list
+        newNode.next = this.head
+        // change the head of the list to the new node
+        this.head = newNode
+        return this.head
+    }
+
+    // Remove Front
+    // Write a method to remove the head node and return the new list head node. If the list is empty, return null.
+    removeFront(){
+        // check to see if there's an object stored at this.head
+        if (this.head){
+            // if so, set it equal to the next object's head in the list, removing the first object from the list
+            this.head = this.next
+        }
+        // return the new object held at head
+        return this.head
+    }
+
+    // Front
+    // Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+    front(){
+        // checks for object stored at this.head
+        if(this.head){
+            // if so, returns the value held for that object
+            return this.head.value
+        }
+        return null
+    }
+}
+
